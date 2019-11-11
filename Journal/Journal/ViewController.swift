424141e01a7250c7f391047ff4a31f87193de2b1
Brandi <blubrandi0@gmail.com>
2019-11-11 15:10:16 -0700

    Created Xcode project

---
+++ Journal/Journal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Journal
+//
+//  Created by BDawg on 11/11/19.
+//  Copyright © 2019 Brandi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
